78bc42a72ecaf920ea8507ba19ac5c1d1e349cc3
Sahil Sharma <vlsharma713@gmail.com>
2024-11-05 22:39:21 +0530

    Create TestBody component

--- src/components/TestcaseSection.jsx
+++ src/components/TestcaseSection.jsx
@@ -1,22 +1,22 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ScrollArea } from "./ui/scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import TestsBody from "./TestsBody";
 
-export default function ProblemSection() {
+export default function TestcaseSection() {
 	return (
-		<Tabs defaultValue="testcase" className="h-full">
+		<Tabs defaultValue="tests" className="h-full">
 			<TabsList className="flex justify-start w-full h-9">
-				<TabsTrigger value="testcase" className="text-sm px-2 py-1">
+				<TabsTrigger value="tests" className="text-sm px-2 py-1">
 					Testcase
 				</TabsTrigger>
 				<TabsTrigger value="test-result" className="text-sm px-2 py-1">
 					Test Result
 				</TabsTrigger>
 			</TabsList>
-			<TabsContent
-				value="testcase"
-				className="h-[calc(100%-36px)] flex flex-col"
-			>
-				<ScrollArea></ScrollArea>
+			<TabsContent value="tests" className="h-[calc(100%-36px)] flex flex-col">
+				<ScrollArea>
+					<TestsBody />
+				</ScrollArea>
 			</TabsContent>
 			<TabsContent value="test-result">
 				<div>You must run your code first</div>
---
+++ src/components/TestsBody.jsx
@@ -0,0 +1,3 @@
+export default function TestsBody() {
+	return <div>Tests here</div>;
+}
